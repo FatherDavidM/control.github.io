c178f780a555b12747a6b0f5586ce34042a11f07
FatherDavidM <murenzvidavid770@gmail.com>
2024-09-12 00:00:24 +0200

    Add files via upload

---
+++ qtcsettings.cmake
@@ -0,0 +1,11 @@
+# This file is managed by Qt Creator, do not edit!
+
+set("CMAKE_GENERATOR" "Ninja" CACHE "STRING" "" FORCE)
+set("CMAKE_BUILD_TYPE" "Release" CACHE "STRING" "" FORCE)
+set("CMAKE_PROJECT_INCLUDE_BEFORE" "C:/Users/USER/Documents/qt/build-cont-WebAssembly_Qt_6_6_2_multi_threaded-Release/.qtc/package-manager/auto-setup.cmake" CACHE "FILEPATH" "" FORCE)
+set("QT_QMAKE_EXECUTABLE" "C:/Qt/ms/6.6.2/wasm_multithread/bin/qmake.bat" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_PREFIX_PATH" "C:/Qt/ms/6.6.2/wasm_multithread" CACHE "PATH" "" FORCE)
+set("CMAKE_C_COMPILER" "C:/Qt/Emsdk/emsdk-3.1.37/upstream/emscripten/emcc.bat" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_COMPILER" "C:/Qt/Emsdk/emsdk-3.1.37/upstream/emscripten/em++.bat" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_TOOLCHAIN_FILE" "C:/Qt/ms/6.6.2/wasm_multithread/lib/cmake/Qt6/qt.toolchain.cmake" CACHE "FILEPATH" "" FORCE)
+set("CMAKE_CXX_FLAGS_INIT" "" CACHE "STRING" "" FORCE)
